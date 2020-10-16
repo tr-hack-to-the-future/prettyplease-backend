95da050cc9c50008d028aa806c52c41fdda0d781
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-16 19:01:51 +0100

    corrected function name in yaml file

--- src/main/java/com/prettyplease/Handler.java
+++ src/main/java/com/prettyplease/Handler.java
@@ -1,35 +1,35 @@
 package com.prettyplease;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.prettyplease.model.Sponsor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-	private static final Logger LOG = LogManager.getLogger(Handler.class);
+    private static final Logger LOG = LogManager.getLogger(Handler.class);
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        LOG.info("received: {}", input);
 
-	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: {}", input);
 
-		Sponsor alfa = new Sponsor(1, "alfa", "Asset finance software");
-		Sponsor blackrock = new Sponsor(1, "blackrock", "Asset management");
+        Sponsor alfa = new Sponsor(21, "alfa", "Asset finance software");
+        Sponsor blackrock = new Sponsor(22, "blackrock", "Asset management");
 
-		List<Sponsor> sponsors = new ArrayList<>();
-		sponsors.add(alfa);
-		sponsors.add(blackrock);
+        List<Sponsor> sponsors = new ArrayList<>();
+        sponsors.add(alfa);
+        sponsors.add(blackrock);
 
-		return ApiGatewayResponse.builder()
-				.setStatusCode(200)
-				.setObjectBody(sponsors)
-				.build();
-	}
+        return ApiGatewayResponse.builder()
+                .setStatusCode(200)
+                .setObjectBody(sponsors)
+                .build();
+    }
+
 }
