ba37e82fb886e82b60f1618d7442dc68fd5280cf
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-16 19:46:24 +0100

    working DB connection

--- src/main/java/com/prettyplease/Handler.java
+++ src/main/java/com/prettyplease/Handler.java
@@ -18,13 +18,34 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
         LOG.info("received: {}", input);
 
-
-        Sponsor alfa = new Sponsor(21, "alfa", "Asset finance software");
-        Sponsor blackrock = new Sponsor(22, "blackrock", "Asset management");
-
+        // create a database connection
+        // create a prepared statement
+        // run it to create a result set
+        // build the sponsors list from the result set
+        // return sponsors
         List<Sponsor> sponsors = new ArrayList<>();
-        sponsors.add(alfa);
-        sponsors.add(blackrock);
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost/prettyplease?"
+                            + "user=user&password=pw");
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM prettyplease.Sponsor");
+
+
+            while (resultSet.next()) {
+                int sponsorId = resultSet.getInt("sponsorId");
+                String name = resultSet.getString("name");
+                String description = resultSet.getString("description");
+                LOG.info("Sponsor: {} - {} - {}", sponsorId, name, description);
+                sponsors.add(new Sponsor(sponsorId, name, description));
+            }
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            LOG.error(e.getMessage());
+        }
 
         return ApiGatewayResponse.builder()
                 .setStatusCode(200)
